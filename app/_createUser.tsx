217d677ee7bfe3208c8cd837d3ca87760301737e
KATT <alexander@n1s.se>
2024-03-27 12:09:08 +0100

    cool

--- app/_createUser.tsx
+++ app/_createUser.tsx
@@ -6,24 +6,37 @@ export type CreateUserState = {
 	errors?: {
 		username?: string;
 	};
-	payload?: FormData;
+	input?: {
+		username?: string;
+	};
 };
 
 export async function createUser(
 	_: CreateUserState,
 	payload: FormData,
 ): Promise<CreateUserState> {
+	console.log("Creating user with payload", payload);
 	// wait 300ms
 	await new Promise((resolve) => setTimeout(resolve, 300));
 	const errors: CreateUserState["errors"] = {};
-	if (!payload.get("username")) {
-		errors.username = "Username is required";
-	}
 	if (payload.get("username") === "john") {
 		errors.username = "Username is already taken";
 	}
 	if (Object.keys(errors).length > 0) {
-		return { errors, payload };
+		// 😷 Some issues:
+		// What I want to do here is to return the errors and the payload so that the form can be re-rendered with the errors and inputs.
+		// return { errors, payload };
+		// 👆❌ This doesn't work because:
+		//    - File is not serializable (makes sense? it should be automatically omitted?)
+		//    - FormData is not serializable (Next.js issue?)
+
+		return {
+			errors,
+			// 😷 Why do I even need to return this?
+			input: {
+				username: payload.get("username") as string,
+			},
+		};
 	}
 
 	redirect("/success");
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,38 @@
 // import { useFormState } from "react-dom";
 import { useFormState } from "react-dom";
 import { createUser } from "./_createUser";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
-function CreateUserForm() {
-	const [state, dispatch] = useFormState(createUser, {});
+function ErrorToastOrBox(props: { children: JSX.Element }) {
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			toast.error(props.children);
+		});
+		return () => clearTimeout(timeout);
+	}, [props.children]);
+
 	return (
-		<form action="/success" className="space-y-4 shadow p-4">
+		<noscript className="text-red p-4 bg-red-200">
+			{props.children}
+		</noscript>
+	);
+}
+function CreateUserForm() {
+	const [state, action] = useFormState(
+		createUser,
+		// 😷 How come I have to define a default state? Default input/payload would make sense for setting default values
+		{},
+	);
+
+	return (
+		<form action={action} className="space-y-4 shadow p-4">
+			{/* errors */}
+			{state.errors && (
+				<ErrorToastOrBox>
+					<>Errors: {JSON.stringify(state.errors, null, 2)}</>
+				</ErrorToastOrBox>
+			)}
 			<div className="flex flex-col space-y-1">
 				<label
 					htmlFor="username"
@@ -20,13 +47,11 @@ function CreateUserForm() {
 					id="username"
 					name="username"
 					placeholder="john"
-					defaultValue={state.payload?.get("username") as string}
+					// 😷 why do I have to return this from the
+					defaultValue={state.input?.username}
 					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					required
 				/>
-				{/* Server validation error */}
-				{state.errors?.username && (
-					<p style={{ color: "red" }}>{state.errors.username}</p>
-				)}
 			</div>
 			<div className="flex flex-col space-y-1">
 				<label
@@ -38,7 +63,6 @@ function CreateUserForm() {
 				<input type="file" id="avatar" name="avatar" accept="image/*" />
 			</div>
 
-			{/* a nice tailwind button */}
 			<button
 				type="submit"
 				className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
