95957df59aa8ba137dc92425c90193faaf1cceb5
KATT <alexander@n1s.se>
2024-03-27 12:46:17 +0100

    go back

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,8 +1,14 @@
 export default function Page() {
 	return (
-		<div>
+		<div className="space-y-4 p-4">
 			<h1>Success</h1>
 			<p>Thank you for submitting the form.</p>
+
+			<p>
+				<a href="/" className="underline">
+					Go back to the form
+				</a>
+			</p>
 		</div>
 	);
 }
